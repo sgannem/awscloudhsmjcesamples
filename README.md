a0047c352a0b5f84b4080876443e470310a8e409
Srinivasu Gannem <srinivasu.gannem@nxp.com>
2020-11-13 07:26:29 +0530

    Initial project setup

--- README.md
+++ README.md
@@ -1,1 +1,124 @@
 # awscloudhsmjcesamples
+
+These sample applications demonstrate how to use the JCE with CloudHSM.
+
+## License Summary
+
+This sample code is made available under a modified MIT license. See the LICENSE file.
+
+## Building the examples
+
+### Dependencies
+
+The CloudHSM Client and JCE dependencies are required. They should be installed using the official
+procedures documented here:
+
+* https://docs.aws.amazon.com/cloudhsm/latest/userguide/java-library-install.html
+
+The examples are tested on a fresh Amazon Linux 2 AMI. You will need to have the following packages 
+installed:
+
+* OpenJDK 8
+* Apache Maven 3.0.5
+
+You can install these packages on Amazon Linux 2 by running
+
+```
+sudo yum install -y java maven
+```
+
+If you are running on Amazon Linux 1, you will need to install extra packages to get Maven.
+You can follow these instructions to build the samples on Amazon Linux 1:
+
+```
+# Maven is only available through extra packages
+sudo wget http://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
+sudo sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
+
+# You will need Java 1.8 to build the samples
+sudo yum install -y java-1.8.0-openjdk-devel
+sudo yum install -y apache-maven
+
+# When updating alternatives, choose the 1.8 path: /usr/lib/jvm/jre-1.8.0-openjdk.x86_64/bin/java
+sudo update-alternatives --config java
+```
+
+
+### Building
+
+You can build the project using Maven. Maven will copy the required CloudHSM jars into a local repository
+and build fat jars which can be executed from the command line. These fat jars will be placed in the
+`target/assembly/` directory. To build the project, use the following command:
+
+```
+mvn validate
+mvn clean package
+```
+
+## Running the samples
+
+You will need to have a CloudHSM Client connected to an ACTIVE cluster. For more details, please follow
+the official instructions here:
+
+* https://docs.aws.amazon.com/cloudhsm/latest/userguide/getting-started.html
+
+All Java dependencies should be bundled in the fat jars. You will only need to specify the location of the
+native library in `/opt/cloudhsm/lib`. Jars can be run using the following command line (as an example): 
+
+```
+java -ea -Djava.library.path=/opt/cloudhsm/lib/ -jar nxpkeydiversification.jar --nxp-div --masterKeyHandle 3932766 --divInput 010490000000013500010000005E88715B --kekHandle 3932766 --user username --password password --partition hsm-xiasgciswmb
+```
+## Output would look like as below.
+```
+#Logging into HSM with user:xxxxxx
+
+Login successful!
+
+#Logged into HSM with user:xxxxx successful...
+# Operating key import operations ...
+#calling getKey({})3932766
+Key handle 3932766 with label nxpsp-test-ck
+Is Key Extractable? : false
+Is Key Persistent? : true
+Key Algo : AES
+Key Size : 128
+#calling getKey({})3932766
+Key handle 3932766 with label nxpsp-test-ck
+Is Key Extractable? : false
+Is Key Persistent? : true
+Key Algo : AES
+Key Size : 128
+TRACE [main] | 2020-11-12 17:44:00.107 | CaviumCipher.java | 536 |   | Enter algorithm AES mode ECB blockSize 16 maxBlocks 1000 aadBlockSize 0 aadMaxBlocks 0
+TRACE [main] | 2020-11-12 17:44:00.108 | CaviumCipher.java | 250 |   | Enter LogId 1 opmode 1
+TRACE [main] | 2020-11-12 17:44:00.108 | CaviumCipher.java | 386 |   | Enter LogId 1
+TRACE [main] | 2020-11-12 17:44:00.108 | CaviumCipher.java | 613 |   | Enter LogId 1 blockSize 16 maxBlocks 1000 aadBlockSize 0 aadMaxBlocks 0
+TRACE [main] | 2020-11-12 17:44:00.109 | CaviumCipher.java | 105 |   | Enter LogId 1 inputOffset 0 inputLen 16
+TRACE [main] | 2020-11-12 17:44:00.110 | CaviumCipher.java | 140 |   | LogId 1 Mode Encrypt doFinal processing 16 bytes, buffer remaining 0
+TRACE [main] | 2020-11-12 17:44:00.113 | CaviumCipher.java | 536 |   | Enter algorithm AES mode CBC blockSize 16 maxBlocks 1000 aadBlockSize 0 aadMaxBlocks 0
+TRACE [main] | 2020-11-12 17:44:00.113 | CaviumCipher.java | 301 |   | Enter LogId 2 opmode 1
+TRACE [main] | 2020-11-12 17:44:00.113 | CaviumCipher.java | 386 |   | Enter LogId 2
+TRACE [main] | 2020-11-12 17:44:00.113 | CaviumCipher.java | 613 |   | Enter LogId 2 blockSize 16 maxBlocks 1000 aadBlockSize 0 aadMaxBlocks 0
+TRACE [main] | 2020-11-12 17:44:00.113 | CaviumCipher.java | 621 |   | LogId 2
+TRACE [main] | 2020-11-12 17:44:00.113 | CaviumCipher.java | 105 |   | Enter LogId 2 inputOffset 0 inputLen 32
+TRACE [main] | 2020-11-12 17:44:00.114 | Paddings.java | 58 |   | Padded input of 32 bytes to 48 bytes
+TRACE [main] | 2020-11-12 17:44:00.114 | CaviumCipher.java | 140 |   | LogId 2 Mode Encrypt doFinal processing 48 bytes, buffer remaining 0
+Entering encryptKey...
+TRACE [main] | 2020-11-12 17:44:00.116 | CaviumCipher.java | 536 |   | Enter algorithm AES mode CBC blockSize 16 maxBlocks 1000 aadBlockSize 0 aadMaxBlocks 0
+TRACE [main] | 2020-11-12 17:44:00.116 | CaviumCipher.java | 301 |   | Enter LogId 3 opmode 1
+TRACE [main] | 2020-11-12 17:44:00.116 | CaviumCipher.java | 386 |   | Enter LogId 3
+TRACE [main] | 2020-11-12 17:44:00.116 | CaviumCipher.java | 613 |   | Enter LogId 3 blockSize 16 maxBlocks 1000 aadBlockSize 0 aadMaxBlocks 0
+TRACE [main] | 2020-11-12 17:44:00.116 | CaviumCipher.java | 621 |   | LogId 3
+TRACE [main] | 2020-11-12 17:44:00.116 | CaviumCipher.java | 105 |   | Enter LogId 3 inputOffset 0 inputLen 16
+TRACE [main] | 2020-11-12 17:44:00.116 | CaviumCipher.java | 140 |   | LogId 3 Mode Encrypt doFinal processing 16 bytes, buffer remaining 0
+Exiting encryptKey...
+#############################################################
+#Got diversifiedKey:3C1BA7D0B794199F38447331F1AE2940
+#Got encryptedDivKey:9E226FB250C504021D80F5BC19B24E34
+#############################################################
+#Now it is logging out ...
+#Logged out from hsm successful...
+```
+
+## Running and verifying all the samples
+
+To run and verify all the samples together, run the command ```mvn verify```
---
+++ src/main/java/com/awscloudhsmjcesamples/NxpKeyDiversification.java
@@ -0,0 +1,437 @@
+/*
+ * Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this
+ * software and associated documentation files (the "Software"), to deal in the Software
+ * without restriction, including without limitation the rights to use, copy, modify,
+ * merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package com.awscloudhsmjcesamples;
+
+import com.cavium.cfm2.CFM2Exception;
+import com.cavium.cfm2.LoginManager;
+import com.cavium.cfm2.Util;
+import com.cavium.key.CaviumAESKey;
+import com.cavium.key.CaviumDES3Key;
+import com.cavium.key.CaviumECPrivateKey;
+import com.cavium.key.CaviumECPublicKey;
+import com.cavium.key.CaviumKey;
+import com.cavium.key.CaviumKeyAttributes;
+import com.cavium.key.CaviumRSAPrivateKey;
+import com.cavium.key.CaviumRSAPublicKey;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.Key;
+import java.security.KeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.Security;
+import java.util.Arrays;
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+
+/**
+ * This sample demonstrates how to work with keys. This could be generating diversified key based diversification
+ * algorithm specified in page no.5 of https://www.nxp.com/docs/en/application-note/AN10922.pdf document.
+ * Algorithm:
+ * 1. Calculate CMAC input D:
+ * 2. D 0x01 || M || Padding
+ * 3. Padding is chosen such that D always has a length of 32 bytes. Padding bytes are
+ * according to the CMAC padding, i.e. 80h followed by 00h bytes. So the length of
+ * Padding is 0 to 30 bytes.
+ * 4. Calculate the Boolean flag ‘Padded’, which is true if M is less than 31 bytes long, false
+ * otherwise. The Boolean argument “Padded” is needed because it must be known in
+ * AES128CMAC which K1 or K2 is to be used in the last computation round.
+ * 5. Calculate output:
+ * 6. Diversified Key AES128CMAC (K, D, Padded)
+ */
+public class NxpKeyDiversification {
+
+    public static final byte BRB = (byte) 0x87; // Rb for AES128
+    public static final String CBC_PKCS5PADDING = "AES/CBC/PKCS5PADDING";
+    public static final String AES_CIPHER_NO_PADDING = "AES/CBC/NoPadding";
+    public static final String ECB_NOPADDING = "AES/ECB/NoPadding";
+    public static final String CAVIUM = "Cavium";
+    public static final byte[] Z16 = new byte[16]; // 128 bit zero
+    public static final IvParameterSpec ZERO_IV = new IvParameterSpec(new byte[16]);
+    public static final char[] HEX = "0123456789ABCDEF".toCharArray();
+
+    private static String helpString = "NxpKeyDiversification\n" +
+            "This sample demonstrates nxp key diversification utility method for working with keys in the " +
+      "HSM.\n" +
+            "\n" +
+            "Options\n"+
+            "\t--nxp-div\t\tTest NXP Key Diversification\n\n"+
+            "\t--masterKeyHandle <masterKeyId(AES128)>\n" +
+            "\t--kekHandle <KEKId(AES128)>\n" +
+            "\t--divInput <divInput>\n" +
+            "\t--user <username>\n" +
+            "\t--password <password>\n" +
+            "\t--partition <partition>\n\n";
+
+    public enum modes {
+        INVALID,
+        NXP_DIV
+    }
+
+    public static void main(String[] args) throws Exception {
+        try {
+            Security.addProvider(new com.cavium.provider.CaviumProvider());
+        } catch (IOException ex) {
+            System.out.println(ex);
+            return;
+        }
+
+        long masterkeyHandle = 0;
+        long kekHandle = 0;
+        String divInput =null;
+        modes mode = modes.INVALID;
+        String user = null;
+        String pass = null;
+        String partition = null;
+        System.out.println("#Got length of inputs params:"+args.length);
+        for (int i = 0; i < args.length; i++) {
+            String arg = args[i];
+            System.out.println("#Got length of input parameter:"+arg);
+            switch (arg) {
+                case "--divInput":
+                    divInput = args[++i];
+                    System.out.println(divInput);
+                    break;
+                case "--masterKeyHandle":
+                    masterkeyHandle = Integer.valueOf(args[++i]);
+                    break;
+                case "--kekHandle":
+                    kekHandle = Integer.valueOf(args[++i]);
+                    break;
+                case "--nxp-div":
+                    mode = modes.NXP_DIV;
+                    break;
+                case "--user":
+                    user = args[++i];
+                    break;
+                case "--password":
+                    pass = args[++i];
+                    break;
+                case "--partition":
+                    partition = args[++i];
+                    break;
+            }
+        }
+
+        if (modes.NXP_DIV == mode && null == divInput && 0 == masterkeyHandle && 0 == kekHandle ) {
+            System.out.println("Please specify one of divInput or masterKeyHandle or kekHandle");
+            help();
+            return;
+        } else {
+            System.out.println("################## EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE #####################");
+        }
+
+
+        /** logging to hsm **/
+        System.out.println("#Logging into HSM with user:" + user);
+        loginWithExplicitCredentials(user, pass, partition);
+        System.out.println("#Logged into HSM with user:" + user + " successful...");
+        System.out.println("# Operating key import operations ...");
+//        // Using the supplied label, find the associated key handle.
+//        // The handle for the *first* key found using the label will be the handle returned.
+//        // If multiple keys have the same label, only the first key can be returned.
+//        if (0 == handle && modes.IMPORT_KEY != mode && modes.IMPORT_PEM != mode) {
+//            System.out.println("#Handle is 0 and mode is:"+mode+", hence finding all label associated key handles...");
+//            try {
+//                long[] handles = {0};
+//                Util.findKey(label, handles);
+//                System.out.println("#Found the following list of handles:"+ Arrays.toString(handles));
+//                handle = handles[0];
+//                System.out.println("#Extracted first key handle:"+handle);
+//            } catch (CFM2Exception ex) {
+//                if (CFM2Exception.isAuthenticationFailure(ex)) {
+//                    System.out.println("Could not find credentials to login to the HSM");
+//                    return;
+//                }
+//
+//                throw ex;
+//            }
+//        }
+//        doProcess(mode, pemFile, handle);
+
+        SecretKey maserKey = (SecretKey) getKey(masterkeyHandle);
+        displayKeyInfo(maserKey);
+        SecretKey kek = (SecretKey)  getKey(masterkeyHandle);
+        displayKeyInfo(kek);
+        byte[] diversifiedKey = doDiversification(maserKey, divInput);
+        byte[] encryptedDivKey = encryptKey(kek, diversifiedKey);
+        System.out.println("#############################################################");
+        System.out.println("#Got diversifiedKey:"+toHexString(diversifiedKey));
+        System.out.println("#Got encryptedDivKey:"+toHexString(encryptedDivKey));
+        System.out.println("#############################################################");
+        System.out.println("#Now it is logging out ...");
+        logout();
+        System.out.println("#Logged out from hsm successful...");
+        /** logging out hsm **/
+
+        /**
+
+
+         **/
+    }
+
+    private static void help() {
+        System.out.println(helpString);
+    }
+
+    public static CaviumKey getKey(long keyHandle) throws KeyException {
+        System.out.println("#calling getKey({})" + keyHandle);
+        try {
+            byte[] keyAttribute = Util.getKeyAttributes(keyHandle);
+            CaviumKeyAttributes cka = new CaviumKeyAttributes(keyAttribute);
+            switch (cka.getKeyType()) {
+                case CaviumKeyAttributes.KEY_TYPE_AES:
+                    return new CaviumAESKey(keyHandle, cka);
+                case CaviumKeyAttributes.KEY_TYPE_DES:
+                case CaviumKeyAttributes.KEY_TYPE_DES3:
+                    return new CaviumDES3Key(keyHandle, cka);
+                case CaviumKeyAttributes.KEY_TYPE_RSA:
+                    switch (cka.getKeyClass()) {
+                        case CaviumKeyAttributes.CLASS_PRIVATE_KEY:
+                            return new CaviumRSAPrivateKey(keyHandle, cka);
+                        case CaviumKeyAttributes.CLASS_PUBLIC_KEY:
+                            return new CaviumRSAPublicKey(keyHandle, cka);
+                        default:
+                            return null;
+                    }
+                case CaviumKeyAttributes.KEY_TYPE_EC:
+                    switch (cka.getKeyClass()) {
+                        case CaviumKeyAttributes.CLASS_PRIVATE_KEY:
+                            return new CaviumECPrivateKey(keyHandle, cka);
+                        case CaviumKeyAttributes.CLASS_PUBLIC_KEY:
+                            return new CaviumECPublicKey(keyHandle, cka);
+                        default:
+                            return null;
+                    }
+                default:
+                    return null;
+            }
+        } catch (CFM2Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static void displayKeyInfo(Key k) {
+        CaviumKey key = (CaviumKey)k;
+        if (null != key) {
+            System.out.printf("Key handle %d with label %s\n", key.getHandle(), key.getLabel());
+            // Display whether the key can be extracted from the HSM.
+            System.out.println("Is Key Extractable? : " + key.isExtractable());
+
+            // Display whether this key is a token key.
+            System.out.println("Is Key Persistent? : " + key.isPersistent());
+
+            // The algorithm and size used to generate this key.
+            System.out.println("Key Algo : " + key.getAlgorithm());
+            System.out.println("Key Size : " + key.getSize());
+        }
+    }
+
+    /**
+     * The explicit login method allows users to pass credentials to the Cluster manually. If you obtain credentials
+     * from a provider during runtime, this method allows you to login.
+     *
+     * @param user      Name of CU user in HSM
+     * @param pass      Password for CU user.
+     * @param partition HSM ID
+     */
+    public static void loginWithExplicitCredentials(String user, String pass, String partition) {
+        LoginManager lm = LoginManager.getInstance();
+        try {
+            lm.login(partition, user, pass);
+            System.out.printf("\nLogin successful!\n\n");
+        } catch (CFM2Exception e) {
+            if (CFM2Exception.isAuthenticationFailure(e)) {
+                System.out.printf("\nDetected invalid credentials\n\n");
+            }
+
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Logout will force the LoginManager to end your session.
+     */
+    public static void logout() {
+        try {
+            LoginManager.getInstance().logout();
+        } catch (CFM2Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static byte[] doDiversification(SecretKey secretKey, String divInput1) {
+            // Step 1
+            Cipher cipher = null;
+            SecretKey skeySpec = secretKey;
+            // 128 bit sub key
+            byte[] k1 = new byte[16];
+            try {
+                cipher = Cipher.getInstance(ECB_NOPADDING, CAVIUM);
+                cipher.init(Cipher.ENCRYPT_MODE, skeySpec);
+                k1 = cipher.doFinal(Z16);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            boolean highL = ((k1[0] & 0x80) != 0);
+            k1 = shl(k1);
+            if (highL) {
+                k1[15] = (byte) (k1[15] ^ BRB);
+            }
+            highL = ((k1[0] & 0x80) != 0);
+
+            // 128 bit sub key
+            byte[] k2 = shl(k1);
+            if (highL) {
+                k2[15] = (byte) (k2[15] ^ BRB);
+            }
+            byte[] diversifiedKey = null;
+            try {
+                diversifiedKey = getDiversifiedKey(divInput1, CAVIUM, skeySpec, k1, k2);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return diversifiedKey;
+        }
+
+    private static byte[] getDiversifiedKey(String divInput, String provider, SecretKey skeySpec, byte[] k1,
+      byte[] k2) throws Exception {
+        Cipher cipher;
+        byte[] xoredD = generateXoredD(divInput, k1, k2);
+        cipher = Cipher.getInstance(CBC_PKCS5PADDING, provider);
+        cipher.init(Cipher.ENCRYPT_MODE, skeySpec, ZERO_IV);
+        byte[] encrypted = cipher.doFinal(xoredD);
+        byte[] dkey = Arrays.copyOfRange(encrypted, 16, 32);
+        return dkey;
+    }
+
+    public static byte[] generateXoredD(String divInput, byte[] k1, byte[] k2) throws IOException {
+        // Step 5
+
+        // Step 6
+        // Step 7
+        byte[] divInput1Bytes = hexStringToByteArray(divInput);
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        outputStream.write(divInput1Bytes);
+
+        // Step 8 & 9
+        byte[] c = outputStream.toByteArray();
+
+        // Step 10 & 11
+        boolean padded = false;
+        if (c.length < 32) {
+            padded = true;
+            byte[] eg = hexStringToByteArray("80");
+            outputStream.write(eg);
+            byte[] st = outputStream.toByteArray();
+
+            byte[] end = new byte[32 - st.length];
+
+            outputStream.write(end);
+
+            c = outputStream.toByteArray();
+            outputStream.close();
+        }
+
+        byte[] xorComponent;
+        if (padded) {
+            xorComponent = k2;
+        } else {
+            xorComponent = k1;
+        }
+
+        int mz = c.length;
+        int n = mz / 16;
+
+        byte[] mLast = new byte[16];
+        int lastn = (n - 1) * 16;
+        int lastz = mz - lastn;
+        System.arraycopy(c, lastn, mLast, 0, lastz);
+        byte[] first = Arrays.copyOfRange(c, 0, 16);
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        stream.write(first);
+        stream.write(xor16(mLast, xorComponent));
+        return stream.toByteArray();
+    }
+
+    public static byte[] shl(byte[] bin) {
+        // << 16 byte array
+        byte[] bout = new byte[16];
+        // java b[0] is the highorder
+        for (short j = 0; j < 15; j++) {
+            int sot = ((bin[j + 1] & 0x80) >> 7);
+            int sef = (bin[j] << 1) | sot;
+            bout[j] = (byte) sef;
+        }
+        bout[15] = (byte) (bin[15] << 1);
+        return bout;
+    }
+
+    public static byte[] hexStringToByteArray(String s) {
+        int len = s.length();
+        byte[] data = new byte[len / 2];
+        for (int i = 0; i < len; i += 2) {
+            data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4) + Character.digit(s.charAt(i + 1), 16));
+        }
+        return data;
+    }
+
+    public static byte[] xor16(byte[] ba, byte[] bb) {
+        byte[] bout = new byte[ba.length];
+        for (short j = 0; j < ba.length; j++) {
+            bout[j] = (byte) (ba[j] ^ bb[j]);
+        }
+        return bout;
+    }
+
+    public static String toHexString(byte[] bytes) {
+        if (null == bytes) {
+            return null;
+        }
+        StringBuilder sb = new StringBuilder(bytes.length << 1);
+        for (int i = 0; i < bytes.length; ++i) {
+            sb.append(HEX[(bytes[i] & 0xf0) >> 4])
+              .append(HEX[(bytes[i] & 0x0f)]);
+        }
+        return sb.toString();
+    }
+
+    private static byte[] encryptKey(Key key, byte[] data) throws Exception {
+        System.out.println("Entering encryptKey...");
+        byte[] kekEncryptedKey = null;
+        Cipher cipher = null;
+        try {
+            cipher = Cipher.getInstance(AES_CIPHER_NO_PADDING, CAVIUM);
+            cipher.init(Cipher.ENCRYPT_MODE, key, new IvParameterSpec(new byte[16]));
+            kekEncryptedKey = cipher.doFinal(data);
+        } catch (Exception e) {
+           e.printStackTrace();
+        }
+        System.out.println("Exiting encryptKey...");
+        return kekEncryptedKey;
+    }
+
+}
